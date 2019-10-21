b1ca8e5738ee4db94d8ea2c5b1518b53c75fc3ef
Ankur Chauhan <ankur.chauhan@auth0.com>
2019-10-21 18:36:20 +0200

    Refine UI styling, add IP + Initial Generator

--- code.js
+++ code.js
@@ -26,9 +26,27 @@ function generatePasswordKey(length) {
     }
     return result;
 }
+function generateIPKey(length) {
+    let result = "";
+    const symbols = "0123456789";
+    const symbolsLength = symbols.length;
+    for (var i = 0; i < length; i++) {
+        result += symbols.charAt(Math.floor(Math.random() * symbolsLength));
+    }
+    return result;
+}
+function generateInitialsKey(length) {
+    let result = "";
+    const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const symbolsLength = symbols.length;
+    for (var i = 0; i < length; i++) {
+        result += symbols.charAt(Math.floor(Math.random() * symbolsLength));
+    }
+    return result;
+}
 figma.showUI(__html__, {
     width: 320,
-    height: 560
+    height: 640
 });
 if (!isText.length)
     figma.closePlugin("Please select a text layer to use the plugin.");
@@ -49,6 +67,22 @@ figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
             yield figma.loadFontAsync(n.fontName);
             if (n.type === "TEXT" && n.characters) {
                 n.characters = generatePasswordKey(msg.value / 2) + generateDataKey(msg.value / 2);
+            }
+        }
+    }
+    else if (msg.type == "generate-ip-key") {
+        for (const n of isText) {
+            yield figma.loadFontAsync(n.fontName);
+            if (n.type === "TEXT" && n.characters) {
+                n.characters = generateIPKey(msg.valueOne) + "." + generateIPKey(msg.valueTwo) + "." + generateIPKey(msg.valueThree) + "." + generateIPKey(msg.valueFour);
+            }
+        }
+    }
+    else if (msg.type == "generate-initials-key") {
+        for (const n of isText) {
+            yield figma.loadFontAsync(n.fontName);
+            if (n.type === "TEXT" && n.characters) {
+                n.characters = generateInitialsKey(msg.value);
             }
         }
     }
--- code.ts
+++ code.ts
@@ -1,79 +1,113 @@
 let isText = filterLayers(
   figma.currentPage.selection,
   i => i.type == "TEXT"
-);
+)
 
 function generateDataKey(length) {
-  let result = "";
+  let result = ""
   const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  const charactersLength = characters.length
   for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
   }
-  return result;
+  return result
 }
 
 function generatePasswordKey(length) {
-  let result = "";
+  let result = ""
   const symbols = "`~!@#$%^&*()-_=+[{]}\|;:',<.>/?"
-  const symbolsLength = symbols.length;
+  const symbolsLength = symbols.length
   for (var i = 0; i < length; i++) {
-    result += symbols.charAt(Math.floor(Math.random() * symbolsLength));
+    result += symbols.charAt(Math.floor(Math.random() * symbolsLength))
   }
-  return result;
+  return result
+}
+
+function generateIPKey(length) {
+  let result = ""
+  const symbols = "0123456789"
+  const symbolsLength = symbols.length
+  for (var i = 0; i < length; i++) {
+    result += symbols.charAt(Math.floor(Math.random() * symbolsLength))
+  }
+  return result
+}
+
+function generateInitialsKey(length) {
+  let result = ""
+  const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const symbolsLength = symbols.length
+  for (var i = 0; i < length; i++) {
+    result += symbols.charAt(Math.floor(Math.random() * symbolsLength))
+  }
+  return result
 }
 
 figma.showUI(__html__, {
   width: 320,
-  height: 560
-});
+  height: 640
+})
 
 if (!isText.length)
-  figma.closePlugin("Please select a text layer to use the plugin.");
+  figma.closePlugin("Please select a text layer to use the plugin.")
 
 figma.ui.postMessage({
 
-});
+})
 
 figma.ui.onmessage = async msg => {
-  console.log(msg);
+  console.log(msg)
 
   if (msg.type == "generate-data-key") {
-    console.log(isText);
+    console.log(isText)
 
     for (const n of isText) {
-      await figma.loadFontAsync(n.fontName as FontName);
+      await figma.loadFontAsync(n.fontName as FontName)
       if (n.type === "TEXT" && n.characters) {
         n.characters = generateDataKey(msg.value)
       }
     }
   } else if (msg.type == "generate-password-key") {
     for (const n of isText) {
-      await figma.loadFontAsync(n.fontName as FontName);
+      await figma.loadFontAsync(n.fontName as FontName)
       if (n.type === "TEXT" && n.characters) {
         n.characters = generatePasswordKey(msg.value / 2) + generateDataKey(msg.value / 2)
       }
     }
+  } else if (msg.type == "generate-ip-key") {
+    for (const n of isText) {
+      await figma.loadFontAsync(n.fontName as FontName)
+      if (n.type === "TEXT" && n.characters) {
+        n.characters = generateIPKey(msg.valueOne) + "." + generateIPKey(msg.valueTwo) + "." + generateIPKey(msg.valueThree) + "." + generateIPKey(msg.valueFour)
+      }
+    }
+  } else if (msg.type == "generate-initials-key") {
+    for (const n of isText) {
+      await figma.loadFontAsync(n.fontName as FontName)
+      if (n.type === "TEXT" && n.characters) {
+        n.characters = generateInitialsKey(msg.value)
+      }
+    }
   } else {
-    figma.closePlugin();
+    figma.closePlugin()
   }
-};
+}
 
 function filterLayers(data, predicate) {
   return !!!data
     ? null
     : data.reduce((list, entry) => {
-        let clone = null;
+        let clone = null
         if (predicate(entry)) {
-          clone = entry;
-          list.push(clone);
+          clone = entry
+          list.push(clone)
         } else if (entry.children != null) {
-          let children = filterLayers(entry.children, predicate);
+          let children = filterLayers(entry.children, predicate)
           if (children.length > 0) {
-            list.push(...children);
+            list.push(...children)
           }
         }
-        return list;
-      }, []);
+        return list
+      }, [])
 }
